84319d8f9018525505f9289e812846725fa39b7a
voidKandy <voidkandy@gmail.com>
2023-07-02 13:44:52 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 pub mod agent;
-pub mod session;
 pub mod tests;
-use agent::agents::*;
-use session::pane::Pane;
 use std::env;
 use tokio;
 
@@ -12,11 +9,4 @@ async fn main() {
         Ok(tmux_var) => println!("📺 Tmux session: {}", tmux_var),
         Err(_) => println!("❗️Make sure your terminal is running inside a Tmux session❗️"),
     }
-
-    // let root = config::Directory::build("test-dir").unwrap();
-    // println!("{}", root);
-    // let rand_content = &root.files[0].content;
-    // println!("{rand_content}");
-    let pane = Pane::capture();
-    pane.write_to("test-dir/test2.txt").unwrap();
 }
