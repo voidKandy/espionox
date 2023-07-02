b73a47bfa4a82213aeb7a9ee694cd9598a37a6c2
voidKandy <voidkandy@gmail.com>
2023-07-02 10:51:46 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,6 @@ async fn main() {
         Ok(tmux_var) => println!("📺 Tmux session: {}", tmux_var),
         Err(_) => println!("❗️Make sure your terminal is running inside a Tmux session❗️"),
     }
-    let handler = AgentHandler::new(SpecialAgent::IoAgent);
-
-    let prompt = handler.get_prompt();
-    // Need to improve function calling
-    let function = &handler
-        .1
-        .functions
-        .as_ref()
-        .unwrap()
-        .get(1)
-        .unwrap()
-        .to_function();
-    let response = handler.1.fn_prompt(&prompt, &function).await.unwrap();
-    let parsed_response = handler.0.parse_response(response);
-    println!("{:?}", parsed_response);
 
     // let root = config::Directory::build("test-dir").unwrap();
     // println!("{}", root);
