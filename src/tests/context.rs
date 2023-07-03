662e8a9d90a906cf4a893792f0585b1788e7e022
voidKandy <voidkandy@gmail.com>
2023-07-02 17:27:07 -0700

    created test for file summarization

--- src/tests/context.rs
+++ src/tests/context.rs
@@ -1,9 +1,10 @@
 #[cfg(test)]
 use crate::agent::context::tmux_session::Pane;
-use crate::agent::context::walk::Directory;
+use crate::agent::context::walk::{Directory, File};
 
 #[allow(dead_code)]
 const TEST_DIRECTORY: &str = "./src/tests/test-dir";
+const TEST_FILE: &str = "./src/tests/test-dir/test2.txt";
 
 #[test]
 fn walk_test() {
@@ -18,6 +19,15 @@ fn walk_test() {
 #[test]
 fn capture_pane_test() {
     let pane = Pane::capture();
-    let response = pane.write_to(&format!("{}/test2.txt", TEST_DIRECTORY));
+    let response = pane.write_to(TEST_FILE);
     assert!(response.is_ok())
+}
+
+#[ignore]
+#[tokio::test]
+async fn summarize_file_test() {
+    let mut root = Directory::build(TEST_DIRECTORY).unwrap();
+    let test_file = &mut root.files[0];
+    test_file.summarize().await.unwrap();
+    assert_ne!("".to_string(), test_file.summary);
 }
