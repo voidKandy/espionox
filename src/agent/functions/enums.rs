6c26785d10807475d4e768167ebde6d47226c6c9
voidKandy <voidkandy@gmail.com>
2023-07-10 13:30:08 -0700

    Update enums.rs

--- src/agent/functions/enums.rs
+++ src/agent/functions/enums.rs
@@ -5,6 +5,8 @@ pub enum FnEnum {
     // GeneralDescription,
     GetCommands,
     RelevantFiles,
+    ProblemSolveTasklist,
+    ExecuteGenerateRead,
 }
 
 impl FnEnum {
@@ -30,6 +32,7 @@ impl FnEnum {
                     perameters,
                 )
             }
+
             FnEnum::RelevantFiles => {
                 let properties = [Property::new(
                     "files",
@@ -40,7 +43,58 @@ impl FnEnum {
                 let perameters = Perameters::new("object", &properties, &["files"]);
                 Function::new(
                     "relevent_files",
-                    "get a list of relevant files from a code snippet",
+                    "Given an error message, get a list of relavent files. Be extremely thorough. If a file is included in the error message, it should be included in the resulting array.",
+                    perameters,
+                )
+            }
+
+            FnEnum::ProblemSolveTasklist => {
+                let properties = [Property::new(
+                    "tasks",
+                    "array",
+                    "a list of fine grained tasks, which by at the end, the problem will be solved",
+                    &[("type", "string"), ("description", "A simple task")],
+                )];
+                let perameters = Perameters::new("object", &properties, &["tasks"]);
+                Function::new(
+                    "problem_solve_tasklist",
+                    "Given a description of a coding problem, it's associated error message, and relevant files, create a list of simple tasks to solve the problem.",
+                    perameters,
+                )
+            }
+
+            FnEnum::ExecuteGenerateRead => {
+                // let properties = [Property::new(
+                //     "tuples",
+                //     "array of tuples: (task, enum(Execute, Generate, Read))",
+                //     "A list of tasks and their classification",
+                //     &[
+                //         ("type", "tuple"),
+                //         (
+                //             "description",
+                //             "a tuple of a task, and either Execute, Generate or Read",
+                //         ),
+                //     ],
+                // )];
+                // let perameters = Perameters::new("object", &properties, &["tuples"]);
+                // Function::new(
+                //     "execute_generate_read",
+                //     "Given the list of tasks classify whether each is a request to generate code, execute a file or shell command, or simply read currently unavailable information.",
+                //     perameters,
+                // )
+                let properties = [Property::new(
+                    "classifications",
+                    "array",
+                    "a list of fine classifications for each of the given tasks",
+                    &[
+                        ("type", "string"),
+                        ("description", "Either Execute, Generate, Read"),
+                    ],
+                )];
+                let perameters = Perameters::new("object", &properties, &["classifications"]);
+                Function::new(
+                    "execute_generate_read",
+                    "Given the list of tasks classify whether each is a request to generate code, execute a file or shell command, or simply read currently unavailable information.",
                     perameters,
                 )
             }
