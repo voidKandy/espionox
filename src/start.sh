d9c063ca710214f2a470f19954dc640ce26240af
voidKandy <voidkandy@gmail.com>
2023-07-10 13:30:10 -0700

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -11,7 +11,5 @@ tmux rename-window -t $session:0 'agent'
 tmux rename-window -t $session:1 'dev'
 
 tmux kill-window -t  $session:2
-# Run cargo run in the Tmux session
-# tmux send-keys -t $session:$window 'cargo run' Enter
 
 tmux attach-session -t $session
