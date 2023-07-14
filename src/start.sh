2cce56f236ff1fd2560988dd7fe1347af27b0434
voidKandy <voidkandy@gmail.com>
2023-07-13 19:36:36 -0700

    Made start.sh more flexible, updating layout

--- src/start.sh
+++ src/start.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+source .env
 
-session="tmux-monitor"
-
+session=$TMUX_SESSION_NAME
 tmux new-session -d -s $session
 
 tmux split-window -h
+tmux run-shell -b -d 0 -t $OUTPUT_PANE 'echo "                                              --Consoxide--"'
+
+tmux new-window 
 tmux new-window 
 
 tmux rename-window -t $session:0 'agent'
 tmux rename-window -t $session:1 'dev'
+tmux rename-window -t $session:2 'watched'
 
-tmux kill-window -t  $session:2
+tmux select-window -t $session:1
+tmux send-keys -t . nv Enter
+
+# tmux kill-window -t  $session:2
 
 tmux attach-session -t $session
