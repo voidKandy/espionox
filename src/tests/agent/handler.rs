0d11a828cbcfc4b76540fdd0699a6af738329933
voidKandy <voidkandy@gmail.com>
2023-07-13 19:37:49 -0700

    Update handler.rs

--- src/tests/agent/handler.rs
+++ src/tests/agent/handler.rs
@@ -1,3 +1,4 @@
+use crate::agent::context::config::{Context, Memory};
 use crate::agent::functions::enums::FnEnum;
 #[allow(unused_imports)]
 use crate::agent::handler::AgentHandler;
@@ -5,7 +6,7 @@ use crate::agent::handler::AgentHandler;
 #[ignore]
 #[tokio::test]
 async fn function_agent_test() {
-    let mut handler = AgentHandler::new();
+    let mut handler = AgentHandler::new(Memory::Temporary);
     let prompt = String::from("[Investigate the failing test in src/tests/context.rs, Check the assertion at line 42 in src/tests/context.rs, Analyze the error message to understand the cause of the failure, Fix the failing test to pass the assertion]");
     handler.context.append_to_messages("user", &prompt);
 
@@ -18,7 +19,7 @@ async fn function_agent_test() {
 #[ignore]
 #[tokio::test]
 async fn watcher_agent_test() {
-    let mut handler = AgentHandler::new();
+    let mut handler = AgentHandler::new(Memory::ShortTerm);
     handler.monitor_user().await;
     assert!(false);
 }
@@ -26,20 +27,31 @@ async fn watcher_agent_test() {
 #[ignore]
 #[tokio::test]
 async fn prompt_agent_test() {
-    let mut handler = AgentHandler::new();
+    let mut handler = AgentHandler::new(Memory::ShortTerm);
     let prompt = String::from("Hello chat agent");
     handler.context.append_to_messages("user", &prompt);
 
     let response = handler.prompt().await;
     assert!(response.is_ok());
+    handler.context.append_to_messages("user", "cool response");
+    let response = handler.prompt().await;
+    handler
+        .context
+        .memory
+        .save_to_short_term(handler.context.messages);
+    assert!(response.is_ok());
 }
 
-#[test]
-fn update_agent_context_test() {
-    let mut handler = AgentHandler::new();
-    let context_before = &handler.context.current_messages().clone();
-    let prompt = String::from("Hello chat agent");
-    handler.context.append_to_messages("user", &prompt);
-
-    assert_ne!(context_before, &handler.context.current_messages());
-}
+// #[test]
+// fn to_and_from_short_term_test() {
+//     let mut handler = AgentHandler::new(Memory::ShortTerm(None));
+//     let prompt = String::from("Hello chat agent");
+//     handler.context.append_to_messages("user", &prompt);
+//     let short_term_mem = Box::new(&handler.context.clone());
+//
+//     handler.context.switch(Memory::Temporary);
+//     assert_ne!(*short_term_mem.messages, &handler.context.messages);
+//     handler
+//         .context
+//         .switch(Memory::ShortTerm(Some(short_term_mem)))
+// }
