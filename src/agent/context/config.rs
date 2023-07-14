fa5c48b6d7951fb832994aa448e3858a796dcce2
voidKandy <voidkandy@gmail.com>
2023-07-13 19:36:58 -0700

    Memory rework.

--- src/agent/context/config.rs
+++ src/agent/context/config.rs
@@ -1,58 +1,100 @@
-use super::tmux_session::Pane;
+use super::tmux_session::TmuxSession;
 use super::walk::{Directory, File};
+use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
+use std::error::Error;
+use std::fs;
+use std::io::prelude::*;
 
-#[derive(Clone, Eq, PartialEq)]
-pub struct Conversation {
+#[derive(Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Context {
     pub messages: Vec<Value>,
-    pub context: Box<Context>,
-    pub pane: Pane,
+    pub memory: Memory,
+    pub session: TmuxSession,
 }
 
 pub trait Contextual {
-    fn make_relevant(&self, context: &mut Conversation) {}
+    fn make_relevant(&self, context: &mut Context);
 }
 
-#[derive(Clone, Eq, PartialEq)]
-pub enum Context {
-    // LongTerm,
-    ShortTerm(Option<Box<Conversation>>),
+#[derive(Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub enum Memory {
+    LongTerm,
+    ShortTerm,
     Temporary,
 }
 
-impl Context {
-    pub fn init(self) -> Box<Conversation> {
+impl Memory {
+    pub const SHORT_TERM_PATH: &str = "./src/agent/context/short_term_memory.json";
+    pub fn init(self) -> Context {
         match self {
-            // _ => self.load(),
-            Context::ShortTerm(Some(conversation)) => conversation,
-            Context::ShortTerm(None) => Conversation::new(vec![], self, Pane::new()),
-            Context::Temporary => Conversation::new(vec![], self, Pane::new()),
+            Memory::LongTerm => {
+                Context::new(self.load_long_term().unwrap(), self, TmuxSession::new())
+            }
+            _ => Context::new(vec![], self, TmuxSession::new()),
         }
+    }
+
+    pub fn load_long_term(&self) -> Result<Vec<Value>, Box<dyn Error>> {
+        unimplemented!();
+    }
+
+    pub fn load_short_term(&self) -> Result<Vec<Value>, Box<dyn Error>> {
+        let mut contents = String::new();
+        fs::File::open(Self::SHORT_TERM_PATH)?.read_to_string(&mut contents)?;
+        println!("{contents}");
+        match serde_json::from_str(&contents) {
+            Ok(Value::Array(array)) => {
+                return Ok(array);
+            }
+            Err(err) => {
+                return Err(format!("Problem getting Json from String: {err:?}").into());
+            }
+            Ok(data) => Ok(vec![data]),
+        }
+    }
+
+    pub fn save_to_short_term(&self, content: Vec<Value>) -> bool {
+        fs::write(
+            Self::SHORT_TERM_PATH,
+            format!(
+                "[{}]",
+                content
+                    .iter()
+                    .map(|m| m.to_string())
+                    .collect::<Vec<_>>()
+                    .join(", "),
+            ),
+        )
+        .unwrap();
+        true
     }
 }
 
-impl Conversation {
-    pub fn new(messages: Vec<Value>, context: Context, pane: Pane) -> Box<Conversation> {
-        Box::new(Conversation {
+impl Context {
+    pub fn new(messages: Vec<Value>, memory: Memory, session: TmuxSession) -> Context {
+        Context {
             messages,
-            context: Box::new(context),
-            pane,
-        })
+            memory,
+            session,
+        }
     }
-    pub fn switch(&mut self, context: Context) {
-        *self = *context.init();
+    pub fn switch(&mut self, memory: Memory) {
+        let new_self = match memory {
+            Memory::ShortTerm => Context::new(
+                memory.load_short_term().unwrap(),
+                memory,
+                TmuxSession::new(),
+            ),
+            _ => {
+                if self.memory == Memory::ShortTerm {
+                    self.memory.save_to_short_term(self.messages.to_owned());
+                }
+                memory.init()
+            }
+        };
+        *self = new_self;
     }
-    // pub fn forget_about(f: fn(Conversation) -> <T>&mut self, ) ->  {
-    //     // if self.messages.keys().all(|k| k != name) {
-    //     //     self.messages.insert(name.to_string(), vec![]);
-    //     // };
-    //     // self.current_conversation = name.to_string();
-    // }
-    // pub fn drop_conversation(&mut self) {
-    //     self.messages.remove(&self.current_conversation);
-    //     let to_convo = self.messages.keys().nth(0).unwrap().to_owned();
-    //     self.change_conversation(&to_convo);
-    // }
     pub fn append_to_messages(&mut self, role: &str, content: &str) {
         self.messages
             .push(json!({"role": role, "content": content}));
@@ -60,7 +102,7 @@ impl Conversation {
 }
 
 impl Contextual for Directory {
-    fn make_relevant(&self, context: &mut Conversation) {
+    fn make_relevant(&self, context: &mut Context) {
         let mut files_payload = vec![];
         self.files.iter().for_each(|f| {
             files_payload.push(match f.summary.as_str() {
@@ -98,7 +140,7 @@ impl Contextual for Directory {
 }
 
 impl Contextual for Vec<File> {
-    fn make_relevant(&self, context: &mut Conversation) {
+    fn make_relevant(&self, context: &mut Context) {
         let mut payload = vec![];
         self.iter().for_each(|f| {
             payload.push(match f.summary.as_str() {
@@ -122,13 +164,13 @@ impl Contextual for Vec<File> {
     }
 }
 
-impl Contextual for Pane {
-    fn make_relevant(&self, context: &mut Conversation) {
+impl Contextual for TmuxSession {
+    fn make_relevant(&self, context: &mut Context) {
         context.append_to_messages(
             "system",
             &format!(
-                "Tmux-Pane:\n name: {}, present directory: {}, contents: [{}]",
-                self.name,
+                "TmuxSession:\n watched_pane: {}, present directory: {}, contents: [{}]",
+                self.watched_pane,
                 self.pwd,
                 self.contents
                     .values()
