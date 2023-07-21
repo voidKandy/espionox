ddd02f3e8c9ec57f93e80a9ad8d64958f464c4b9
voidKandy <voidkandy@gmail.com>
2023-07-20 17:54:47 -0700

    SHORT_TERM_PATH env variable

--- src/agent/config/memory.rs
+++ src/agent/config/memory.rs
@@ -3,6 +3,7 @@ use super::super::handler::context::Context;
 
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::env;
 use std::error::Error;
 use std::fs;
 use std::io::prelude::*;
@@ -15,7 +16,6 @@ pub enum Memory {
 }
 
 impl Memory {
-    pub const SHORT_TERM_PATH: &str = "./src/agent/config/short_term_memory.json";
     pub fn init(self) -> Context {
         match self {
             Memory::LongTerm => {
@@ -31,7 +31,7 @@ impl Memory {
 
     pub fn load_short_term(&self) -> Result<Vec<Value>, Box<dyn Error>> {
         let mut contents = String::new();
-        fs::File::open(Self::SHORT_TERM_PATH)?.read_to_string(&mut contents)?;
+        fs::File::open(env::var("SHORT_TERM_PATH").unwrap())?.read_to_string(&mut contents)?;
         println!("{contents}");
         match serde_json::from_str(&contents) {
             Ok(Value::Array(array)) => {
@@ -46,7 +46,7 @@ impl Memory {
 
     pub fn save_to_short_term(&self, content: Vec<Value>) -> bool {
         fs::write(
-            Self::SHORT_TERM_PATH,
+            env::var("SHORT_TERM_PATH").unwrap(),
             format!(
                 "[{}]",
                 content
