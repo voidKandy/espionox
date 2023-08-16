d49a54a75ad74bd38660054ce84d1ce9cf81732e
voidKandy <voidkandy@gmail.com>
2023-08-16 16:28:25 -0700

    Basic autocomplete for the wrong stuff

--- src/interface.rs
+++ src/interface.rs
@@ -1,30 +1,62 @@
 use crate::handler::agent::Agent;
 use crate::handler::operations::Operational;
-use inquire::Text;
+use inquire::{
+    autocompletion::{Autocomplete, Replacement},
+    CustomUserError, Text,
+};
 
 pub struct Ui<'a> {
     completer: HistoryCompleter,
     agent: &'a mut Agent,
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Clone)]
 pub struct HistoryCompleter {
     input: String,
     history: Vec<String>,
 }
 
 impl HistoryCompleter {
-    pub fn update(&mut self, str: &str) {
+    pub fn update_input(&mut self, input: &str) -> Result<(), CustomUserError> {
+        if input == self.input {
+            return Ok(());
+        }
+
+        Ok(self.input = input.to_owned())
+    }
+    pub fn update_history(&mut self, str: &str) {
         self.history.push(str.to_string())
     }
 }
 
+impl Autocomplete for HistoryCompleter {
+    fn get_suggestions(&mut self, input: &str) -> Result<Vec<String>, CustomUserError> {
+        self.update_input(input)?;
+        Ok(self.history.clone())
+    }
+
+    fn get_completion(
+        &mut self,
+        input: &str,
+        highlighted_suggestion: Option<String>,
+    ) -> Result<Replacement, CustomUserError> {
+        self.update_input(input)?;
+        Ok(match highlighted_suggestion {
+            Some(suggestion) => Replacement::Some(suggestion),
+            None => Replacement::None,
+        })
+    }
+}
 impl<'a> Ui<'a> {
     pub fn init(agent: &'a mut Agent) -> Self {
         Ui {
             completer: HistoryCompleter::default(),
             agent,
         }
+    }
+
+    pub fn completer(&self) -> impl Autocomplete {
+        self.completer.clone()
     }
 
     fn greet() {
@@ -82,8 +114,11 @@ impl<'a> Ui<'a> {
     }
 
     fn prompt_user(&mut self) -> String {
-        let input = Text::new("").prompt().unwrap();
-        self.completer.update(&input);
+        let input = Text::new("")
+            .with_autocomplete(self.completer())
+            .prompt()
+            .unwrap();
+        self.completer.update_history(&input);
         input.to_string()
     }
 
