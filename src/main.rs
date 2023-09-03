da44fef1693dfcd9f909fd4509b9a39e7b44ab48
voidKandy <voidkandy@gmail.com>
2023-09-03 16:25:09 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use consoxide::{handler::AgentSettings, *};
+use consoxide::{
+    agent::{Agent, AgentSettings},
+    interface::Ui,
+};
 use tokio;
 
 #[tokio::main]
 async fn main() {
-    let mut agent = handler::Agent::build(AgentSettings::default()).expect("Failed to build agent");
-    interface::Ui::init(Some(&mut agent)).interractive_loop();
+    let mut agent = Agent::build(AgentSettings::default()).expect("Failed to build agent");
+    Ui::init(Some(&mut agent)).interractive_loop();
 }
