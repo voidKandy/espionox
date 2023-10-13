1408c123c5345c5f62eade751d1bd865b9e62093
voidKandy <voidkandy@gmail.com>
2023-10-13 09:47:17 -0700

    Configuration no longer uses config crate

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,34 +1,12 @@
 #[cfg(feature = "long_term_memory")]
-use serde_aux::field_attributes::deserialize_number_from_string;
-use std::path::PathBuf;
+use serde_yaml;
+use std::{collections::BTreeMap, fs, io::Read, path::PathBuf};
+
+use crate::language_models::openai::gpt::GptModel;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct ConfigEnv {
     config_file_name: String,
-}
-
-#[derive(serde::Deserialize, Clone, Debug)]
-pub struct GlobalSettings {
-    pub language_model: LanguageModelSettings,
-    #[cfg(feature = "long_term_memory")]
-    pub database: DatabaseSettings,
-}
-
-#[derive(serde::Deserialize, Clone, Debug)]
-pub struct LanguageModelSettings {
-    // pub model: String,
-    pub api_key: String,
-}
-
-#[cfg(feature = "long_term_memory")]
-#[derive(serde::Deserialize, Clone, Debug)]
-pub struct DatabaseSettings {
-    #[serde(deserialize_with = "deserialize_number_from_string")]
-    pub port: u16,
-    pub username: String,
-    pub password: String,
-    pub host: String,
-    pub database_name: String,
 }
 
 impl Default for ConfigEnv {
@@ -39,14 +17,214 @@ impl Default for ConfigEnv {
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct GlobalSettings {
+    pub language_model: LanguageModelSettings,
+    pub database: Option<DatabaseSettings>,
+}
+
+struct GlobalSettingsBuilder {
+    lm: Option<LanguageModelSettings>,
+    db: Option<DatabaseSettings>,
+}
+
+#[derive(Clone, Debug)]
+pub struct LanguageModelSettings {
+    pub default_model: GptModel,
+    pub api_key: String,
+}
+
+#[derive(Clone, Debug)]
+pub struct DatabaseSettings {
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub host: String,
+    pub database_name: String,
+}
+
+type SettingsYamlMap = BTreeMap<String, String>;
+type GlobalSettingsYamlMap = BTreeMap<String, SettingsYamlMap>;
+
+fn global_yaml_map_from_path(path: PathBuf) -> GlobalSettingsYamlMap {
+    let mut file = fs::File::open(path).expect("Failed to read default config path");
+    let mut content = String::new();
+    file.read_to_string(&mut content)
+        .expect("Failed to read file");
+    let yaml_map: GlobalSettingsYamlMap = serde_yaml::from_str(&content).unwrap();
+    yaml_map
+}
+
+trait OverwriteWith<T> {
+    fn overwrite_with(&mut self, _: T) {}
+}
+
+impl OverwriteWith<SettingsYamlMap> for LanguageModelSettings {
+    fn overwrite_with(&mut self, map: SettingsYamlMap) {
+        for (k, val) in map {
+            match k.as_str() {
+                "model" => {
+                    self.default_model = val.try_into().unwrap();
+                }
+                "api_key" => {
+                    self.api_key = val;
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
 #[cfg(feature = "long_term_memory")]
-impl std::fmt::Display for DatabaseSettings {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "Database Url: postgres://{}:{}@{}:{}/{}",
-            self.username, self.password, self.host, self.port, self.database_name
-        )
+impl OverwriteWith<SettingsYamlMap> for DatabaseSettings {
+    fn overwrite_with(&mut self, map: SettingsYamlMap) {
+        for (key, val) in map {
+            let val = val.to_string();
+            match key.as_str() {
+                "port" => {
+                    self.port = val.parse().unwrap();
+                }
+                "username" => {
+                    self.username = val;
+                }
+                "password" => {
+                    self.password = val;
+                }
+                "host" => {
+                    self.host = val;
+                }
+                "database_name" => {
+                    self.database_name = val;
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+impl OverwriteWith<GlobalSettingsYamlMap> for GlobalSettings {
+    fn overwrite_with(&mut self, global_map: GlobalSettingsYamlMap) {
+        for (key, map) in global_map {
+            match key.as_str() {
+                "language_model" => self.language_model.overwrite_with(map),
+
+                "database" => {
+                    if let Some(database) = self.database.as_mut() {
+                        database.overwrite_with(map);
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+impl TryFrom<&SettingsYamlMap> for LanguageModelSettings {
+    type Error = anyhow::Error;
+    fn try_from(map: &SettingsYamlMap) -> Result<Self, Self::Error> {
+        let mut model: Option<GptModel> = None;
+        let mut api_key: Option<String> = None;
+        for (key, val) in map {
+            let val = val.to_string();
+            match key.as_str() {
+                "model" => {
+                    model = Some(val.try_into().unwrap());
+                }
+                "api_key" => {
+                    api_key = Some(val);
+                }
+                _ => {}
+            }
+        }
+        let default_model = match model {
+            None => {
+                tracing::info!("No model in language model settings, using default");
+                GptModel::default()
+            }
+            Some(m) => m,
+        };
+        Ok(LanguageModelSettings {
+            default_model,
+            api_key: api_key.ok_or_else(|| anyhow::anyhow!("Missing api key"))?,
+        })
+    }
+}
+
+impl TryFrom<&SettingsYamlMap> for DatabaseSettings {
+    type Error = anyhow::Error;
+    fn try_from(map: &SettingsYamlMap) -> Result<Self, Self::Error> {
+        let mut port: Option<u16> = None;
+        let mut username: Option<String> = None;
+        let mut password: Option<String> = None;
+        let mut host: Option<String> = None;
+        let mut database_name: Option<String> = None;
+        for (key, val) in map {
+            let val = val.to_string();
+            match key.as_str() {
+                "port" => {
+                    port = Some(val.parse().unwrap());
+                }
+                "username" => {
+                    username = Some(val);
+                }
+                "password" => {
+                    password = Some(val);
+                }
+                "host" => {
+                    host = Some(val);
+                }
+                "database_name" => {
+                    database_name = Some(val);
+                }
+                _ => {}
+            }
+        }
+        Ok(DatabaseSettings {
+            port: port.ok_or_else(|| anyhow::anyhow!("Port is missing"))?,
+            username: username.ok_or_else(|| anyhow::anyhow!("Username is missing"))?,
+            password: password.ok_or_else(|| anyhow::anyhow!("Password is missing"))?,
+            host: host.ok_or_else(|| anyhow::anyhow!("Host is missing"))?,
+            database_name: database_name
+                .ok_or_else(|| anyhow::anyhow!("Database name is missing"))?,
+        })
+    }
+}
+
+impl TryFrom<GlobalSettingsYamlMap> for GlobalSettingsBuilder {
+    type Error = anyhow::Error;
+    fn try_from(yaml_map: GlobalSettingsYamlMap) -> Result<Self, Self::Error> {
+        let mut global_settings_builder = GlobalSettings::build();
+        for (setting, map) in yaml_map.iter() {
+            match setting.as_str() {
+                "database" => {
+                    global_settings_builder.db = Some(DatabaseSettings::try_from(map).unwrap());
+                }
+                "language_model" => {
+                    global_settings_builder.lm =
+                        Some(LanguageModelSettings::try_from(map).unwrap());
+                }
+                _ => {}
+            }
+        }
+        Ok(global_settings_builder)
+    }
+}
+
+impl TryInto<GlobalSettings> for GlobalSettingsBuilder {
+    type Error = anyhow::Error;
+    fn try_into(self) -> Result<GlobalSettings, Self::Error> {
+        Ok(GlobalSettings {
+            language_model: self
+                .lm
+                .ok_or_else(|| anyhow::anyhow!("Language model settings missing"))?,
+            database: self.db,
+        })
+    }
+}
+
+impl GlobalSettings {
+    fn build() -> GlobalSettingsBuilder {
+        GlobalSettingsBuilder { lm: None, db: None }
     }
 }
 
@@ -70,38 +248,41 @@ impl ConfigEnv {
     }
 
     #[tracing::instrument(name = "Get settings from environment")]
-    pub fn get_settings(&self) -> Result<GlobalSettings, config::ConfigError> {
+    pub fn global_settings(&self) -> Result<GlobalSettings, anyhow::Error> {
         let default_config_path = Self::default().config_file_path();
-        let mut default_config_override: Option<PathBuf> = None;
-        match self.config_file_name.as_str() {
+        let default_config_override: Option<PathBuf> = match self.config_file_name.as_str() {
             "default" => {
-                tracing::info!("Using default database configuration");
+                tracing::info!("Using default path");
+                None
             }
-            _ => {
-                let filepath = self.config_file_path();
-                tracing::info!(
-                    "Using configuration from {}",
-                    filepath.display().to_string()
-                );
-                default_config_override = Some(filepath);
+            path => {
+                tracing::info!("Using override path: {}", path);
+                Some(self.config_file_path())
             }
-        }
-        let config = config::Config::builder().add_source(config::File::from(default_config_path));
+        };
+
+        let df_map = global_yaml_map_from_path(default_config_path);
+        let mut df_settings: GlobalSettings = GlobalSettingsBuilder::try_from(df_map)
+            .unwrap()
+            .try_into()
+            .unwrap();
         if let Some(path) = default_config_override {
-            let config = config.add_source(config::File::from(path)).build()?;
-            let settings = config
-                .try_deserialize::<GlobalSettings>()
-                .expect("Failed to build custom settings");
-            #[cfg(feature = "long_term_memory")]
-            tracing::info!("Database url from settings: \n{}", settings.database);
-            return Ok(settings);
+            let override_map = global_yaml_map_from_path(path);
+            df_settings.overwrite_with(override_map);
         }
-        let config = config.build()?;
-        let settings = config
-            .try_deserialize::<GlobalSettings>()
-            .expect("Failed to build settings");
-        #[cfg(feature = "long_term_memory")]
-        tracing::info!("Database url from settings: \n{}", settings.database);
-        Ok(settings)
+
+        tracing::info!("Got default global settings: {:?}", df_settings);
+        Ok(df_settings)
+    }
+}
+
+#[cfg(feature = "long_term_memory")]
+impl std::fmt::Display for DatabaseSettings {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Database Url: postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
     }
 }
